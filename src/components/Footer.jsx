578b3c12bf3fbd18363c05585f230879192a6876
Jivan-Plata <jivanplata@gmail.com>
2025-10-19 20:04:21 +0800

    Update footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ import githubLogo from '../assets/github-icon.png'
 export default function Footer() {
     return (
         <footer>
-            <a href = "www.x.com/@jivnplatss"> <img src = {twitterLogo} alt = "Twitter Logo" /></a>
-            <a href = "www.facebook.com/jivan.plata"> <img src = {facebookLogo} alt = "Facebook Logo" /> </a>
-            <a href = "www.instagram.com/ji.vannn"><img src = {instagramLogo} alt = "Instagram Logo" /></a>
-            <a href = "www.github.com/Jivan-Plata"><img src = {githubLogo} alt = "Github Logo" /> </a>
+            <a href = "https://www.x.com/@jivnplatss"> <img src = {twitterLogo} alt = "Twitter Logo" /></a>
+            <a href = "https://www.facebook.com/jivan.plata"> <img src = {facebookLogo} alt = "Facebook Logo" /> </a>
+            <a href = "https://www.instagram.com/ji.vannn"><img src = {instagramLogo} alt = "Instagram Logo" /></a>
+            <a href = "https://www.github.com/Jivan-Plata"><img src = {githubLogo} alt = "Github Logo" /> </a>
         </footer>
     )
 }
