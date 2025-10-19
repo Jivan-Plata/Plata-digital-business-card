98feea40deb0a6353c5541f73bcf97cae065d58b
Jivan-Plata <jivanplata@gmail.com>
2025-10-19 19:46:33 +0800

    Fixing undetected images test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
+import twitterLogo from '../assets/twitter-icon.png'
+
 export default function Footer() {
     return (
         <footer>
-            <a href = "www.x.com/@jivnplatss"> <img src = "./src/assets/Twitter Icon.png" /></a>
+            <a href = "www.x.com/@jivnplatss"> <img src = {twitterLogo} /></a>
             <a href = "www.facebook.com/jivan.plata"> <img src = "./src/assets/Facebook Icon.png" /> </a>
-            <a href = "www.instagram.com/ji.vannn"><img src = "./src/assets/Instagram Icon.png" /></a>
-            <a href = "www.github.com/Jivan-Plata"><img src = "./src/assets/GitHub Icon.png" /> </a>
+            <a href = "www.instagram.com/ji.vannn"><img src = "./src/assets/instagram-icon.png" /></a>
+            <a href = "www.github.com/Jivan-Plata"><img src = "./src/assets/gitHub-icon.png" /> </a>
         </footer>
     )
 }
